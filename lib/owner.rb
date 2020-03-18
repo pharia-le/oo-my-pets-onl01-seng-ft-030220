72f78e0fccbb304052e9428cf029d4413c541a0d
Pharia Le <pharia.le@gmail.com>
2020-03-18 03:44:45 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,53 @@
+require 'pry'
+
 class Owner
-  # code goes here
+  
+  attr_accessor 
+  attr_reader :name, :species
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name  
+    @species = "human"
+    self.save
+  end
+  
+  def say_species
+    "I am a #{self.species}."
+  end
+  
+  def cats
+    Cat.all.select {|cat| cat.owner == self}
+  end
+  
+  def dogs
+    Dog.all.select {|dog| dog.owner == self}
+  end
+  
+  def buy_cat(name)
+    binding.pry
+    Cat.all.find {|cat| cat.name == name}.owner = self
+  end
+  
+  # def buy_dog(name)
+  #   Dog.all.find {|dog| dog.name == name}.owner = self
+  # end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.count
+    self.all.size
+  end
+  
+  def self.reset_all
+    self.all.clear  
+  end
+  
+  def self.all
+    @@all  
+  end
+  
 end
